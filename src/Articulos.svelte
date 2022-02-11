7c2e33feef5e482facd1fcf92b9cfeba06aa93bd
Juan Antonio González <jgonzalezrivera@eci.safanet.es>
2022-02-11 13:29:20 +0100

    reactive search

--- src/Articulos.svelte
+++ src/Articulos.svelte
@@ -2,18 +2,27 @@
     import { getContext, onMount } from "svelte";
     import { jsonData } from "./store";
     import Articulo from "./Articulo.svelte";
+    import Buscar from "./Buscar.svelte";
 
     const URL = getContext("URL");
-
+    let busqueda = "";
+    
     onMount(() => {
         fetch(URL.articulos)
             .then((response) => response.json())
             .then((data) => ($jsonData = data));
     });
+
+$: regexp = new RegExp (busqueda, "i");
+$: datos = busqueda 
+    ? $jsonData.filter((i) => regexp.test(i.nombre)) 
+    : $jsonData
 </script>
 
 <h1>ARTICULOS</h1>
 
-{#each $jsonData as articulo}
-    <Articulo articulo = {articulo}/>
+<Buscar bind:busqueda />
+
+{#each datos as articulo}
+    <Articulo {articulo} />
 {/each}
